6cb6b77e1ab807da971ddfb33f227780cace7670
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2024-04-09 01:53:05 +0545

    orientation changed

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import {ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+
+async function changeScreenOrientation() {
+  await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+}
 
 export default function App() {
+ changeScreenOrientation();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('D:\Pygame Projects\js-juttpatti\src\assets\sprites\Board.png')} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Inside lllllllll</Text>
+      </ImageBackground>
+      <StatusBar hidden/>
     </View>
   );
 }
@@ -14,7 +23,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
   },
 });
